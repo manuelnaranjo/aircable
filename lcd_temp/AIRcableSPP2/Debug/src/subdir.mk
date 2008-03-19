0c289ee090574a3eb7c48caeff53913061341e1c
naranjo.manuel <naranjo.manuel@424b5fdb-2024-0410-8daf-076db069837d>
2008-03-19 20:54:02 +0000

    AIRcableSPP2 tagged 0.2.0, added Debug makefile to svn

---
+++ lcd_temp/AIRcableSPP2/Debug/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/main.c \
+../src/node.c \
+../src/types.c 
+
+OBJS += \
+./src/main.o \
+./src/node.o \
+./src/types.o 
+
+C_DEPS += \
+./src/main.d \
+./src/node.d \
+./src/types.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I/usr/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
