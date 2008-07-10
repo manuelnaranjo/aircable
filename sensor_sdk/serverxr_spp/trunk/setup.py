28bee238c0a03ce3aa145426e3fc2937aabda4f0
naranjo.manuel <naranjo.manuel@424b5fdb-2024-0410-8daf-076db069837d>
2008-07-10 22:17:04 +0000

    SensorSDK: spp server rename, added setup.py for it

---
+++ sensor_sdk/serverxr_spp/trunk/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='AIRcableSPP',
+	version='0.1',
+	description='AIRcable SDKSensors SPP Server',
+	author='Naranjo Manuel Francisco',
+	author_email='manuel@aircable.net',
+	package_dir= {'' : 'src' } ,
+	url='www.aircable.net',
+	packages=['net.aircable.spp'],
+	license="Apache version 2",
+    )
+    
