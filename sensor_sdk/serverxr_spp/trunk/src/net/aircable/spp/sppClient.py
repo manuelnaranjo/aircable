93833845d5543e72742513e98517f748c2cc4cc8
naranjo.manuel <naranjo.manuel@424b5fdb-2024-0410-8daf-076db069837d>
2008-07-12 01:03:48 +0000

    SensorSDK, sppClient: Added a client that can make connections, tomorrow we test sending an receiving data on the client, so then we can work on the server

--- sensor_sdk/serverxr_spp/trunk/src/net/aircable/spp/sppClient.py
+++ sensor_sdk/serverxr_spp/trunk/src/net/aircable/spp/sppClient.py
@@ -1,56 +1,148 @@
-from socket import *
+"""
+    A simple class for connecting to spp servers.
+
+    Copyright 2008 Wireless Cables Inc. <www.aircable.net>
+    Copyright 2008 Naranjo, Manuel Francisco <manuel@aircable.net>
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+import socket
+
+from re import compile
+from errors import *
+
+__pattern = compile(r'.*\n');
+
+def getDefaultDeviceAddress():
+    pass
 
 class sppClient:
-	__socket  = None;
+	'''
+	    Simple wrapper for an spp client, actually it is an rfcomm client, 
+	    not only spp.
+	'''
+        __socket  = None;
 	__address  = None;
-	__channel = None;
+        __channel = None;
 	__service = None;
+	__device  = None;
 	
-	define __init__(self, socket):
-		self.__socket = socket;
+	def __init__(self, socket):
+	    '''
+		    You use this constructor when you all ready have a socket
+	        
+		arguments:
+		    socket descriptor
+	    '''
+	    self.__socket = socket;
 
-	define __init__(self, address, channel=-1, service='spp'):
-		self.__address = address;
-		self.__channel = channel;
-		self.__service = service;
+	def __init__( self,
+			target  = '', 
+			channel = -1, 
+			service = 'spp',
+			device  = getDefaultDeviceAddress() ):
+	    '''
+		More general constructor. You will use this one when you want
+		sppClient to make the conneciton.
+	    
+		arguments:
+	    	    target:  Bluetooth Address of the device you want to connect to,
+		    channel: Channel to be used for establishing the connection.
+		    service: Service to use when you want sppClient to do service
+			     Discovery.
+		    device:  Bluetooth Address of the local device you want to 
+			     use for making the connection.
+	    '''
+	    
+	    self.__address = target;
+	    self.__channel = int(channel);
+	    self.__service = service;
+	    self.__device  = device;
 
 	def connect(self):
-		if self.__channel < 1:
-			System.err.println("Service resolving not implemented" +
-					" yet....");
-			return;
-
-		if (self.__socket == None):
-			self.__socket = socket( AF_BLUETOOTH, SOCK_STREAM, 
-					BTPROTO_RFCOMM );
+	    '''
+		Starts the connection
+	    '''
+	    if self.__channel < 1:
+		    raise SPPNotImplemented, 'Profiles resolving isn\'t implemented yet'
+		    
+	    if (self.__socket == None):
+		print 'creating socket'    
+		self.__socket = socket.socket( socket.AF_BLUETOOTH, 
+						socket.SOCK_STREAM, 
+						socket.BTPROTO_RFCOMM );
+	    #Let BlueZ decide outgoing port
+	    print 'binding to %s, %i' % ( self.__device , 0 )
+	    self.__socket.bind( (self.__device,0) );
 		
-		self.connect(self.__address, self.__channel);
+	    print 'connecting to %s, %i' % ( self.__address, self.__channel )
+	    self.__socket.connect( (self.__address, self.__channel) );
 
-	def __checkConnected(self):
+	def __checkConnected(self, message =''):
 		if self.__socket == None:
-			System.err.println("Not connected");
-			return
+		    raise SPPNotConnectedException, message
 
 	def send(self, text):
-		self.__checkConnected();
+	    '''
+		Send something to the port.
+	    
+		Arguments: what to send
+	    '''
+	    self.__checkConnected('Can\'t send if not connected');
 		
-		self.__socket.sendall(text);
+	    self.__socket.sendall(text);
 
 	def read(self, bytes=10):
-		self.__checkConnected();
+	    '''
+		Read binary data from the port.
+	    
+		Arguments:
+		    bytes: Amount of bytes to read
+	    '''
+	    self.__checkConnected('Can\'t read if not connected');
 		
-		return self.__socket.recv(bytes);
+	    return self.__socket.recv(bytes);
 
-	__pattern = compile(r'.*\n');
+	
 
 	def readLine(self):
-		self.__checkConnected();
+	    '''
+		Reads until \n is detected
+	    '''
+	    out = buffer("");
 		
-		out = buffer("");
-		
-		while ( 1 ):
-			out += self.read(1);
+	    while ( 1 ):
+		out += self.read(1);
 
-			if self.__pattern.match(out):
-				return replace(out, '\n', '');
+		if __pattern.match(out):
+			return replace(out, '\n', '');
 
+if __name__ == '__main__':
+    import sys
+    
+    if ( len(sys.argv) < 4 ):
+	print "Usage %s <target> <channel> <adapter>" % (sys.argv[0])
+	sys.exit(1)
+
+    a = sppClient(
+	    target	=sys.argv[1],
+	    channel	=sys.argv[2],
+	    device	=sys.argv[3]
+	);
+    
+    a.connect()
+    
+    a.readLine();
+    
+    a.send('1234');
