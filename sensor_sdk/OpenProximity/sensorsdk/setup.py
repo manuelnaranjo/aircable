bd8213247a877c9eeddd0b9e7aa55dfe95e4591d
naranjo.manuel <naranjo.manuel@424b5fdb-2024-0410-8daf-076db069837d>
2009-11-24 23:46:52 +0000

    added a working setup.py

---
+++ sensor_sdk/OpenProximity/sensorsdk/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name="SensorSDK",
+    version="0.1",
+    packages=['sensorsdk',],
+    summary="SensorSDK core",
+    description="""SensorSDK OpenProximity plugin""",
+    long_description="""this plugin allows OpenProximity to be used for remote bluetooth sensing networks.
+It provides plugin with basic stuff such: data storage, charting, data sync, etc.
+SensorSDK is a plugin system it self, sensors talk to SensorSDK through plugins""",
+    author="Naranjo Manuel Francisco",
+    author_email= "manuel@aircable.net",
+    package_dir={'sensorsdk': 'sensorsdk'},
+    package_data={'sensorsdk': ['templates/sensorsdk/*.html',]},
+    license="GPL2",
+    url="http://code.google.com/p/aircable/", 
+)
