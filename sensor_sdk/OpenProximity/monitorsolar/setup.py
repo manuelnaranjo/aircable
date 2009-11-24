c71308fb849232511c65ace17bfd5570b9333f0a
naranjo.manuel <naranjo.manuel@424b5fdb-2024-0410-8daf-076db069837d>
2009-11-24 23:47:29 +0000

    added egg for solar panel

---
+++ sensor_sdk/OpenProximity/monitorsolar/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="SensorSDK Solar Sensor",
+    version="0.1",
+    packages=['monitorsolar',],
+    summary="SensorSDK Solar Sensor",
+    description="""SensorSDK plugin""",
+    long_description="""A sample plugin for solar watter heatter monitoring""",
+    author="Naranjo Manuel Francisco",
+    author_email= "manuel@aircable.net",
+    license="GPL2",
+    url="http://code.google.com/p/aircable/", 
+)
