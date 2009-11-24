217596e9ae838a6bc96367bd4d5461630fefdc8b
naranjo.manuel <naranjo.manuel@424b5fdb-2024-0410-8daf-076db069837d>
2009-11-24 23:47:05 +0000

    added setup.py for monitorambient

--- sensor_sdk/OpenProximity/monitorambient/setup.py
+++ sensor_sdk/OpenProximity/monitorambient/setup.py
@@ -1,6 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(name="Monitor Ambient",
+setup(name="SensorSDK Ambient Sensor",
     version="0.1",
     packages=['monitorambient',],
+    summary="SensorSDK Ambient Sensor",
+    description="""SensorSDK plugin""",
+    long_description="""A sample plugin for ambient monitoring""",
+    author="Naranjo Manuel Francisco",
+    author_email= "manuel@aircable.net",
+    license="GPL2",
+    url="http://code.google.com/p/aircable/", 
 )
