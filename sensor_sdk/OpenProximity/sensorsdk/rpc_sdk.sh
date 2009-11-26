89eb89eae3e04f5b562ad61f55fa0ff01e9c22a5
naranjo.manuel <naranjo.manuel@424b5fdb-2024-0410-8daf-076db069837d>
2009-11-26 00:21:12 +0000

    added sh script for sensorsdk

---
+++ sensor_sdk/OpenProximity/sensorsdk/rpc_sdk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# this script will launch all the needed parts for an OpenProximity2.0 stand
+# alone server
+
+PYTHONPATH=$(pwd)/libs
+LOG_DIR=/var/log/aircable
+LOG_FILE=$LOG_DIR/scanner.log
+
+export PYTHONPATH
+export LOG_FILE
+
+cd serverXR
+echo "Starting RPC Scanner Client"
+if [ -z "$DEBUG" ]; then
+    exec python manager.py localhost 8010 sensorsdk 2>&1 &
+else
+    exec python manager.py localhost 8010 sensorsdk
+fi
