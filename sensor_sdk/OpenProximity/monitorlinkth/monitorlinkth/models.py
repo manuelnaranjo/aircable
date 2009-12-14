43684c5b381b6d0b47e2d412c2b399e28ae3b734
naranjo.manuel <naranjo.manuel@424b5fdb-2024-0410-8daf-076db069837d>
2009-12-14 20:16:56 +0000

    modified:   monitorlinkth/monitorlinkth/models.py modified:   monitorlinkth/setup.py * Moved from NEXT to NEXT4 * Dynamic classes

--- sensor_sdk/OpenProximity/monitorlinkth/monitorlinkth/models.py
+++ sensor_sdk/OpenProximity/monitorlinkth/monitorlinkth/models.py
@@ -28,8 +28,35 @@ from re import compile
 from datetime import datetime
 import math
 
-class LinkTHDevice(models.SensorSDKRemoteDevice):
+MAXSENSORS = 5
+
+SENSOR_FAMILY = {
+    ''  : '18S20 or 18B20',
+    '00': 'MS-T',
+    '19': 'MS-TH',
+    '1A': 'MS-TV',
+    '1B': 'MS-TL',
+}
+
+SENSOR_EXTRACTION={
+    #temp only
+    '': lambda x: {'temperature': float(x.split(',')[1])},
+    '00': lambda x: {'temperature': float(x.split(',')[1])},
+
+    #temp humidity
+    '19': lambda x: {'temperature': float(x.split(',')[1]), 
+			'extra': float(x.split(',')[3])},
+
+    #temp voltage
+    '1A': lambda x: {'temperature': float(x.split(',')[1]), 
+			'extra': float(x.split(',')[3])},
     
+    #temp light
+    '1B': lambda x: {'temperature': float(x.split(',')[1]), 
+			'extra': float(x.split(',')[3])},
+}
+
+class LinkTHDevice(models.SensorSDKRemoteDevice):
     @staticmethod
     def getMode():
 	'''Let the sensorsdk engine know which node we are handling'''
@@ -37,31 +64,62 @@ class LinkTHDevice(models.SensorSDKRemoteDevice):
 
     @staticmethod
     def getChartVariables():
-	return ['temperature_c', 'battery']
-	
+	fields=list()
+	for i in range(MAXSENSORS):
+	    fields.append('value%s' % i,)
+	    fields.append('value%sa' % i,)
+	fields.append('battery',)
+	return fields
+
     @staticmethod
     def getRecordClass():
         return LinkTHRecord
 
-owi=compile(r'OWI\|(?P<temp_c>[-+]?[0-9]*\.?[0-9]+).*')
-class LinkTHRecord(models.SensorSDKRecord):
-    temperature_c = mod.FloatField(help_text=_('readed temperature in celsius'))
+    def findSensorForId(self, dev_id):
+	j = 0
+	for i in range(MAXSENSORS):
+	    if dev_id==getattr(self, 'sensor%s_id' % i, None):
+		return i
+	    j+=1
+	if j < MAXSENSORS:
+	    return j
+	raise Exception("Sensor not found, no more slots available")
+	
+for i in range(MAXSENSORS):
+    LinkTHDevice.add_to_class(
+	'sensor%s_id' % i, 
+	mod.CharField(max_length=16,
+	    help_text=_('sensor %s ID') % i,
+	    null=True, blank=True,
+	)
+    )
+    LinkTHDevice.add_to_class(
+	'sensor%s_name' %i, 
+	mod.CharField(max_length=100,
+	    help_text=_('sensor %s friendly name') % i,
+	    null=True, blank=True,
+	)
+    )
+    LinkTHDevice.add_to_class(
+	'sensor%s_family' %i,
+	mod.CharField(max_length=100,
+	    help_text=_('sensor %s family based on ID') % i,
+	)
+    )
+
+LINE=r'OWI\|(?P<id>[A-F][0-9]+)\|OWS\|(?P<val>.*)\|(?P<rest>.*)$'
+LINE=compile(LINE)
     
+class LinkTHRecord(models.SensorSDKRecord):
+
     @staticmethod
     def getMode():
 	'''Let the sensorsdk engine know which node we are handling'''
 	return "monitor-linkth"
-    
+
     @staticmethod
     def parsereading(device=None, seconds=None, battery=None, reading=None, dongle=None):
 	'''This method expects to get a valid reading, generating a record out of it'''
-	
-	#extract parameters from reading string
-	m = owi.match(reading)
-	if not m:
-	    print "NO MATCH", reading
-	    return
-	m=m.groupdict()
 	
 	#find ambient device, or create if there's none yet created
 	device,created=LinkTHDevice.objects.get_or_create( address=device,
@@ -70,11 +128,58 @@ class LinkTHRecord(models.SensorSDKRecord):
 		'sensor': _('Linkth'),
 		'mode': _('Monitor'),
 	    })
-
+	
 	record = LinkTHRecord()
 	record.remote=device
 	record.dongle=dongle
-	record.temperature_c=float(m['temp_c'])
 	record.time=datetime.fromtimestamp(seconds)
-	record.battery=battery
+	record.battery=battery/1000 # asume we get battery * 1000
+	
+	while len(reading) > 0:
+	    m = LINE.match(reading)
+	    if not m:
+		print "monitorlinkth NO MATCH", reading
+		return
+	    m=m.groupdict()
+	    reading = m['rest']
+	    val = m['val']
+	    typ = val.split(',')[0].strip()
+	    
+	    try:
+		sen_id = device.findSensorForId(m['id'])
+	    except Exception, err:
+		# if we got here then we have more than MAXSENSORS
+		# registered on this linkth
+		print err
+		break
+	    
+	    if not getattr(device, 'sensor%s_id' % sen_id, None):
+		setattr(device, 'sensor%s_id' % sen_id, m['id'])
+		setattr(device, 'sensor%s_name' % sen_id, _("Auto discovered 1wire sensor"))
+		setattr(device, 'sensor%s_family' % sen_id, SENSOR_FAMILY[typ])
+		device.save()
+
+	    val = SENSOR_EXTRACTION[typ](val)
+	    setattr(record, 'value%s' % sen_id, val['temperature'])
+	    
+	    if typ in ['19', '1A', '1B']:
+		setattr(record, 'value%sa' % send_id, val['extra'])
+
 	record.save()
+
+for i in range(MAXSENSORS):
+    LinkTHRecord.add_to_class(
+	'value%s' % i, 
+	mod.FloatField(
+	    help_text=_('temperature meassured from sensor number %s in celsius') % i,
+	    null=True, blank=True,
+	)
+    )
+    LinkTHRecord.add_to_class(
+	'value%sa' %i, 
+	mod.FloatField(
+	    help_text=_('optional reading from sensor number %s friendly name') % i,
+	    null=True, blank=True,
+	)
+    )
+
