c4adaae58ce78079f38265f5254d775baaebc8c4
naranjo.manuel <naranjo.manuel@424b5fdb-2024-0410-8daf-076db069837d>
2010-01-15 17:03:59 +0000

    Added last_records view, send friendly_name over json
    modified:   sensorsdk/urls.py
    * added url for last_records
    
    modified:   sensorsdk/views.py
    * added last_records view
    * added get_subclass method
    * added props method which takes a django object and generates a dict out of it.
    * pass friendly_name on get_sensors

--- sensor_sdk/OpenProximity/sensorsdk/sensorsdk/views.py
+++ sensor_sdk/OpenProximity/sensorsdk/sensorsdk/views.py
@@ -21,13 +21,55 @@ from django.conf import settings
 from django.utils.translation import ugettext as _
 from django.utils import simplejson
 from django.shortcuts import render_to_response
-import models
+from wadofstuff.django.serializers.base import Serializer
+import models, inspect
 
 SET = settings.OPENPROXIMITY.getAllSettings()
 
 def get_modes(request):
     return HttpResponse(
 	simplejson.dumps(list(models.SensorSDKRemoteDevice.getModes())),
+	content_type='application/json')
+
+def get_subclass(object):
+    for related in object._meta.get_all_related_objects():
+	if type(object) in related.model._meta.get_parent_list():
+	    if hasattr(object,related.var_name):
+		return get_subclass(getattr(object, related.var_name))
+    return object
+
+def props(obj):
+    # based on http://stackoverflow.com/questions/61517/python-dictionary-from-an-objects-fields
+    pr = {}
+    for field in obj._meta.local_fields:
+	name = field.name
+	if name.endswith('_ptr'):
+	    continue
+        value = getattr(obj, name)
+        if not name.startswith('__') and not inspect.ismethod(value):
+            pr[name] = value
+    return pr
+
+def last_records():
+    qs=models.SensorSDKRemoteDevice.objects.order_by('latest_served')
+    
+    for dev in qs.all():
+	t = {
+	    'name': dev.name,
+	    'mode': dev.mode,
+	    'sensor': dev.sensor,
+	    'address': dev.address,
+	    'friendly_name': dev.friendly_name,
+	    'latest_served': str(dev.latest_served),
+	}
+	q = models.SensorSDKRecord.objects.filter(remote=dev)
+	if q.count() > 0:
+	    t['last_record'] = props(get_subclass(q.latest('time')))
+	yield t
+    
+def get_last_records(request):
+    return HttpResponse(
+	simplejson.dumps(list(last_records())),
 	content_type='application/json')
 
 def get_sensors(request, mode=None):
@@ -44,6 +86,7 @@ def get_sensors(request, mode=None):
 		'mode': i.mode,
 		'sensor': i.sensor,
 		'address': i.address.replace(':','_'),
+		'friendly_name': i.friendly_name,
 	}
 
     return HttpResponse(simplejson.dumps(out),
