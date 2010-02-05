cb3c2878372f1cbefddc8ab2ccafb3541311e82d
naranjo.manuel <naranjo.manuel@424b5fdb-2024-0410-8daf-076db069837d>
2010-02-05 16:31:22 +0000

    added more validation information

--- sensor_sdk/OpenProximity/sensorsdk/sensorsdk/forms.py
+++ sensor_sdk/OpenProximity/sensorsdk/sensorsdk/forms.py
@@ -1,23 +1,34 @@
 from django import forms
-from django.forms import fields, models, widgets
+from django.forms import fields, models, widgets, ValidationError
+from django.forms.util import ErrorList
 from django.http import HttpResponseRedirect
 from django.contrib.formtools.wizard import FormWizard
 from django.core.urlresolvers import reverse
 from django.utils.encoding import force_unicode
 from django.utils.translation import ugettext as _
+from django.utils.safestring import mark_safe
 import models
+from urlparse import urlparse
 
-from re import compile
+class ServerField(forms.CharField):
+    def clean(self, value):
+	if len(urlparse(value).path) > 0:
+	    return value
+	raise ValidationError(_("%s not matching a valid Server url") % value)
+
+class BooleanField(forms.BooleanField):
+    def is_checkbox(self):
+	return True
 
 class EmailForm(forms.Form):
     DEFAULT_FROM_EMAIL = forms.EmailField(
 	label=_("Email sender"), 
 	required=False,
-	help_text=_("When an alert is triggered this will be the sender email.<br>Example: alert@opensensors.org")
+	help_text=_("When an alert is triggered this will be the sender email. <b>Example</b>: alert@opensensors.org")
     )
-    EMAIL_HOST = forms.URLField(
+    EMAIL_HOST = ServerField(
 	label=_("Email server"),
-	help_text=_("Your SMTP email server address.<br>Example: smtp.google.com")
+	help_text=_("Your SMTP email server address. <b>Example</b>: smtp.google.com")
     )
     EMAIL_HOST_USER = forms.CharField(
 	label=_("Email server user"), 
@@ -32,45 +43,67 @@ class EmailForm(forms.Form):
     EMAIL_PORT = forms.IntegerField(
 	label=_("Email server port"), 
 	initial=25,
-	help_text=_("Some servers don't provide the SMTP service at the regular port (25)<br>ask your SMTP provider for the port number.<br>Example Gmail: 587"),
+	help_text=_("Some servers don't provide the SMTP service at the regular port (25) ask your SMTP provider for the port number.<b>Example</b> 587 for <b>Gmail</b>"),
 	min_value=1,
 	max_value=(2**16)-1
     )
-    EMAIL_USE_TLS = forms.BooleanField(
+    EMAIL_USE_TLS = BooleanField(
 	label=_("Should use TLS for this server?"), 
 	initial=False, 
 	required=False,
-	help_text=_("Some servers like for example GMail require some content encryption<br>check this option if you need TLS encryption")
+	help_text=_("Some servers like Gmail require content encryption check this option if you need TLS encryption")
     )
     
+    def clean_EMAIL_PORT(self):
+	value=self.cleaned_data['EMAIL_PORT']
+	if int(value) <= 0:
+	    raise ValidationException(_("Port number invalid"))
+    
+    def clean(self):
+	if 'EMAIL_HOST_USER' in self.cleaned_data and \
+	    (not 'EMAIL_HOST_PASSWORD' in self.cleaned_data or \
+		len(self.cleaned_data['EMAIL_HOST_PASSWORD'].strip())==0):
+	    self._errors['EMAIL_HOST_PASSWORD']=ErrorList([_("You need to set a password if you set a username")])
+	return self.cleaned_data
+    
 class AlertTemplateForm(forms.Form):
-    NO_DATA = forms.BooleanField(
+    NO_DATA = BooleanField(
 	label=_("No data"),
 	initial = False,
 	required = False,
-	help_text=("No data template is used when a known SensorSDK device stops reporting")
+	help_text=_("No data template is used when a known SensorSDK device stops reporting")
     )
-    OVER_RANGE = forms.BooleanField(
+    OVER_RANGE = BooleanField(
 	label=_("Over Range"),
 	initial = False,
 	required = False,
-	help_text=("Over Range template is used when a certain variable from a known SensorSDK reports been over a certain treshold")
+	help_text=_("Over Range template is used when a certain variable from a known SensorSDK reports been over a certain treshold")
     )
-    UNDER_RANGE = forms.BooleanField(
+    UNDER_RANGE = BooleanField(
 	label=_("Under Range"),
 	initial = False,
 	required = False,
-	help_text=("Under Range template is used when a certain variable from a known SensorSDK reports been under a certain treshold")
+	help_text=_("Under Range template is used when a certain variable from a known SensorSDK reports been under a certain treshold")
     )
-    IN_RANGE = forms.BooleanField(
+    IN_RANGE = BooleanField(
 	label=_("In Range"),
 	initial = False,
 	required = False,
-	help_text=("Under Range template is used when a certain variable from a known SensorSDK reports been between two certain values")
+	help_text=_("Under Range template is used when a certain variable from a known SensorSDK reports been between two certain values")
     )
-    ACCEPT = forms.BooleanField(
+    ACCEPT = BooleanField(
 	label=_("Save Settings"),
 	initial = False,
 	required = False,
-	help_text=("If you don't mark this field then settings will not be saved")
+	help_text=_("If you don't mark this field then settings will not be saved")
     )
+    
+    def clean(self):
+	if ( self.cleaned_data['NO_DATA'] or \
+	    self.cleaned_data['OVER_RANGE'] or \
+	    self.cleaned_data['UNDER_RANGE'] or \
+	    self.cleaned_data['IN_RANGE'] ) and not self.cleaned_data['ACCEPT']:
+		self._errors['ACCEPT']=ErrorList([
+		    mark_safe(_('No changes are saved as <b>Save Settings</b> wasn\'t selected')),
+		    ])
+	return self.cleaned_data
